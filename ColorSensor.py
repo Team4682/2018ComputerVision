7d8f88cc175bbddb17ab8c95a4f79621f9e7fd4d
Team4682 <35178499+Team4682@users.noreply.github.com>
2018-02-17 22:07:46 -0800

    Update ColorSensor.py

--- ColorSensor.py
+++ ColorSensor.py
@@ -7,36 +7,43 @@ import smbus
 import Adafruit_TCS34725 as af
 import os
 
+#Define the Global Variables:
+#IP address and SD tables
+#Initialize the Server as well.
+'''-----------------------------------------------------------------------'''
 ip = "10.46.82.2"
 nt.initialize(server=ip)
-
-
+sc = nt.getTable('Scale')
+sw = nt.getTable('Switch')
+'''------------------------------------------------------------------------'''
 #Create an instance of the Color Sensor
 sensor = af.TCS34725()
 
 sensor.set_interrupt(False)
 
+#Detect Color and Calculate Lumosity in infinite loop
+'''------------------------------------------------------------------'''
 while(1):
     
     r, g, b, c = sensor.get_raw_data()
     time.sleep(0.5)
     print("rgb=("+str(r)+", "+str(g)+", "+str(b)+")")
-    #print(r)
-    #print(g)
-    #print(b)
-    cls()
+    
     colorTemp = af.calculate_color_temperature(r, g,b)
     lux = af.calculate_lux(r,g,b)
     print(str(colorTemp) + "k")
     print(str(lux) + " lux")
-    
-
-
-
-
-print(colorTemp)
-print(lux)
-
+    if lux <= 9:
+        sc.putNumber('View', 0)
+        print("Lumosity is %s, Robot is not in the Scale Area" % (str(lux)))
+    elif lux =< 10:
+        sc.putNumber('View', 1)
+        print("Lumosity is %s, Robot is in the Scale Area" % ((str(lux)))
+'''---------------------------------------------------------------------'''
+              
+              
+'''-----------------------------------------------------------------------'''
+              #Enter sleep mode for Sensor
 sensor.set_interrupt(True)
 sensor.disable()
 
